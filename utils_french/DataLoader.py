a62b729ffe26bdb782f4e065f9c057b779709f44
Raoufdine SAID <said.raoufdine@gmail.com>
2023-08-26 00:48:26 +0200

    use french tool version

---
+++ utils_french/DataLoader.py
@@ -0,0 +1,112 @@
+import random
+import numpy as np
+import torch
+
+class DataLoader():
+    def __init__(self, dataset, batch_size=1, shuffle=False, ner=False, prop=None):
+        assert batch_size > 0
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.lidx = list(range(len(self.dataset)))
+        self.ner = ner
+        self.prop = prop
+        
+        if self.prop is not None:
+            assert self.prop >= 0
+            assert self.prop <= 1
+
+        # Padding last batch if necessary
+        if len(self.lidx) % self.batch_size != 0:
+            self.lidx = self.lidx + random.sample(self.lidx, self.batch_size - (len(self.lidx) % self.batch_size))
+
+        # Shuffle if necessary
+        if self.shuffle:
+            random.shuffle(self.lidx)
+
+    def __getitem__(self, idx):
+        assert idx >= 0
+        if idx == 0:
+            self.lidx = list(range(len(self.dataset)))
+
+            # Padding last batch if necessary
+            if len(self.lidx) % self.batch_size != 0:
+                self.lidx = self.lidx + random.sample(self.lidx, self.batch_size - (len(self.lidx) % self.batch_size))
+
+            # Shuffle if necessary
+            if self.shuffle:
+                random.shuffle(self.lidx)
+
+        if self.prop is not None:
+            if (idx >= self.prop * (len(self.lidx) / self.batch_size)):
+                return self.dataset[len(self.dataset)]
+
+        if (idx >= len(self.lidx) / self.batch_size):
+            return self.dataset[len(self.dataset)]
+
+        idxs = self.lidx[idx*self.batch_size:idx*self.batch_size+self.batch_size]
+        batch = [self.dataset[i] for i in idxs]
+
+        batch = self.merge(batch)
+
+        return batch
+
+    def __len__(self):
+        if self.prop is not None:
+            return int(self.prop * len(self.lidx) / self.batch_size)
+
+        return int(len(self.lidx) / self.batch_size)
+
+    def merge(self, batch):
+        list_input_ids = []
+        list_attention_mask = []
+        list_labels_sum = []
+        list_labels_ner = []
+
+        def merge_list(l):
+            res = []
+            for e in l:
+                res = res + e
+            return res
+
+        idxs = [e["idx"] for e in batch]
+
+        if "labels" in batch[0]:
+            max_doc_len = max([len(e["input_ids"]) for e in batch])
+            for block in range(max_doc_len):
+                line = []
+
+                for e in batch:
+                    if len(e["input_ids"]) > block:
+                        line.append(e)
+
+                list_input_ids.append(torch.cat([e["input_ids"][block].unsqueeze(0) for e in line], dim=0))
+                list_attention_mask.append(torch.cat([e["attention_mask"][block].unsqueeze(0) for e in line]))
+
+                list_labels_sum.append(merge_list([e["labels"][block] for e in line]))
+            
+            #list_labels_sum = merge_list(list_labels_sum)
+            #list_labels_ner = merge_list(list_labels_ner)
+
+            return {"idx" : idxs, "input_ids" : list_input_ids, "attention_mask": list_attention_mask, "labels" : list_labels_sum}
+            
+
+        max_doc_len = max([len(e["input_ids"]) for e in batch])
+        for block in range(max_doc_len):
+            line = []
+
+            for e in batch:
+                if len(e["input_ids"]) > block:
+                    line.append(e)
+
+            list_input_ids.append(torch.cat([e["input_ids"][block].unsqueeze(0) for e in line], dim=0))
+            list_attention_mask.append(torch.cat([e["attention_mask"][block].unsqueeze(0) for e in line]))
+
+            list_labels_sum.append(merge_list([e["labels_sum"][block] for e in line]))
+            list_labels_ner.append(merge_list([e["labels_ner"][block] for e in line]))
+        
+        #list_labels_sum = merge_list(list_labels_sum)
+        #list_labels_ner = merge_list(list_labels_ner)
+
+        return {"idx" : idxs, "input_ids" : list_input_ids, "attention_mask": list_attention_mask, "labels_sum" : list_labels_sum, "labels_ner" : list_labels_ner}
+        
---
+++ utils_french/preprocess_text.py
@@ -0,0 +1,90 @@
+# URLs:
+# * preprocessing 1 : https://towardsdatascience.com/nlp-preprocessing-with-nltk-3c04ee00edc0
+# * preprocessing 2 : https://www.nltk.org/api/nltk.tokenize.html
+
+import torch
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import nltk
+# Run the below line only the first time of running nltk
+# nltk.download()
+
+def preprocess_text(text, sent_tokenizer, bert_tokenizer, block_size, entities=None, trunc_doc=-1):
+  # list of input_ids and list of attention_mask
+  list_input_ids = []
+  list_attention_mask = []
+  list_labels_entities = []
+
+  # tokenize sentence
+  tokenized_text = sent_tokenizer(text, language="french")
+
+  if trunc_doc >= 0:
+    tokenized_text = tokenized_text[:trunc_doc]
+
+  # build list of input_ids without overflow for each input_ids
+  y = 0
+  while y < len(tokenized_text):
+    text = (" " + bert_tokenizer.sep_token + " ").join(tokenized_text[y:]) + (" " + bert_tokenizer.sep_token + " ")
+
+    inputs = bert_tokenizer.encode_plus(
+      text,
+      add_special_tokens=False,
+      padding="max_length",
+      max_length=block_size,
+      return_tensors="pt",
+      truncation=True
+    )
+
+    input_ids = inputs['input_ids'].squeeze()
+
+    n = torch.sum(torch.eq(input_ids, bert_tokenizer.sep_token_id)).item()
+
+    # Very long sentence, we will trunc it
+    if n == 0:
+      n = 1
+      input_ids[-1] = bert_tokenizer.sep_token_id
+      attention_mask = inputs['attention_mask'].squeeze()
+    else:
+      text = (" " + bert_tokenizer.sep_token + " ").join(tokenized_text[y:y+n]) + (" " + bert_tokenizer.sep_token + " ")
+
+      inputs = bert_tokenizer.encode_plus(
+        text,
+        add_special_tokens=False,
+        padding="max_length",
+        max_length=block_size,
+        return_tensors="pt",
+        truncation=True
+      )
+
+      input_ids = inputs['input_ids'].squeeze()
+      attention_mask = inputs['attention_mask'].squeeze()
+
+    list_input_ids.append(input_ids)
+    list_attention_mask.append(attention_mask)
+    y += n
+
+  # Compute NER labels
+  if entities is not None:
+    for input_ids in list_input_ids:
+      linput_ids = input_ids[torch.ne(input_ids, bert_tokenizer.pad_token_id) & torch.ne(input_ids, bert_tokenizer.sep_token_id)].tolist()
+      labels_entities = [0 for _ in range(len(linput_ids))]
+      entities = sorted(entities, key=len, reverse=True)
+      for entity in entities:
+        entity_ids = bert_tokenizer.encode(entity, add_special_tokens=False)
+        zeros = [0 for _ in entity_ids]
+        for x, id in enumerate(linput_ids):
+          if len(entity_ids) == len(linput_ids[x:x+len(entity_ids)]) and entity_ids == linput_ids[x:x+len(entity_ids)] and labels_entities[x:x+len(entity_ids)] == zeros:
+            if len(entity_ids) >= 2:
+              for i in range(len(entity_ids)):
+                labels_entities[x+i] = 1#'C'
+              labels_entities[x] = 1#'L'
+              labels_entities[x+len(entity_ids) - 2] = 1#'R'
+            else:
+              labels_entities[x] = 1#'E'
+    
+      list_labels_entities.append(labels_entities)
+
+    return list_input_ids, list_attention_mask, list_labels_entities
+  
+  return list_input_ids, list_attention_mask
